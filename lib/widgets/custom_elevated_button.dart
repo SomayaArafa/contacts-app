e5a8fcf5717e0932b81b6df71eb386dd1cf799b5
somaya arafa <@gmail>
2026-01-02 23:21:58 +0200

    create elevated button

---
+++ lib/widgets/custom_elevated_button.dart
@@ -0,0 +1,32 @@
+import 'package:contacts_app/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomElevatedButton extends StatelessWidget {
+  const CustomElevatedButton({super.key, this.onPressed, required this.title});
+  final void Function()? onPressed;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: onPressed,style:ElevatedButton.styleFrom(
+      backgroundColor: AppColors.gold,
+      foregroundColor: AppColors.darkBlue,
+      padding: const EdgeInsets.all(16),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      textStyle: const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w400,
+      ),
+    ).copyWith(
+      overlayColor: WidgetStateProperty.all(Colors.transparent),
+    ),
+      child:  Center(
+      child: Text(
+     title,
+      textAlign: TextAlign.center,
+    ),
+    ));
+
+  }
+}
