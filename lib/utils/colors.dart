3a2051037c7e72af73f7e6645e53f8e5360bf0bf
somaya arafa <@gmail>
2026-01-02 23:01:43 +0200

    create text field

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
-abstract final class AppColors{
-  static  const Color darkBlue=Color(0xff29384D);
-  static  const Color gold=Color(0xffFFFFFF);
-  static  const Color white=Color(0xffFFFFFF);
-  static  const Color lightBlue=Color(0xffFFF1D4);
+abstract final class AppColors {
+  static const Color darkBlue = Color(0xff29384D);
+  static const Color gold = Color(0xffFFF1D4);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color lightBlue = Color(0xffE2F4F6);
+  static const Color red = Color(0xffEE403D);
 
 }
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,3 +1,4 @@
+import 'package:contacts_app/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
@@ -9,11 +10,52 @@ class CustomTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  TextField(
-      decoration: InputDecoration(
-        hintText:hintText,
-
+    return TextFormField(
+      keyboardType: textInputType,
+      cursorColor: AppColors.gold,
+      style: const TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        color: AppColors.white,
       ),
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: AppColors.darkBlue,
+        hintText: hintText,
+        hintStyle: const TextStyle(
+          color: AppColors.lightBlue,
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+        ),
+        contentPadding: const EdgeInsets.all(16),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: const BorderSide(
+            color: AppColors.gold,
+            width: 2,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: const BorderSide(
+            color: AppColors.gold,
+            width: 2,
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: const BorderSide(
+            color: AppColors.red,
+            width: 2,
+          ),
+        ),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'This field is required';
+        }
+        return null;
+      },
     );
   }
 }
